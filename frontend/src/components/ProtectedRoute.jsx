a0692da728b79f73134e5d4d9cf9af7333ff8d57
Judy Khaled <20P6630@eng.asu.edu.eg>
2025-05-16 03:47:51 +0300

    list events for admin

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 // src/components/ProtectedRoute.jsx
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ roleRequired, children }) {
+export default function ProtectedRoute({ roleRequired = [], children }) {
   const token = localStorage.getItem("token");
   const role = localStorage.getItem("role");
 
-  if (!token || role !== roleRequired) {
+  if (!token || !roleRequired.includes(role)) {
+    alert("❌ Unauthorized access. Redirecting to login.");
     return <Navigate to="/login" replace />;
   }
 
